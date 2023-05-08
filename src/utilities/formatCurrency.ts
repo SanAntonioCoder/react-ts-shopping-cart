5697b3e87b31b444f5a27a738d887b53198ceff8
SanAntonioCoder <SanAntonioCoder@gmail.com>
2023-05-08 10:37:11 -0500

    new branch netlify dev test

--- src/utilities/formatCurrency.ts
+++
@@ -1,8 +0,0 @@
-const CURRENCT_FORMATTER = new Intl.NumberFormat(undefined, {
-  currency: "USD",
-  style: "currency",
-});
-
-export function formatCurrency(number: number) {
-  return CURRENCT_FORMATTER.format(number);
-}
